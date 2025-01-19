59deb3e12b93538385a306cca284a46d456b12c4
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-19 17:06:25 +0330

    added post model to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'counted_views', 'status',
+                    'published_date', 'created_date', 'updated_date')
+    list_filter = ('status', 'created_date')
+    search_fields = ('title', 'content')
+    actions = ['make_published']
+    empty_value_display = '-empty-'
+    date_hierarchy = 'created_date'
