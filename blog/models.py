2756331ec4a0eece279dcd7f7db0689639a588ab
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-19 17:06:09 +0330

    created Post model and made migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f'{self.title} - {self.id}'
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("blog:single", kwargs={"pid": self.id})
