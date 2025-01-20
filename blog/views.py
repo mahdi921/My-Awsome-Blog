d65d9b40886e3011c590e431f01fe9d02b8b3cda
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-20 18:24:05 +0330

    added the functionality to view all published posts on blog page added pagination on blog page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
 # Create your views here.
 
 def blog_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now(),
                                 status=1)
+    posts = Paginator(posts, 6)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
     context = {
-        'psots' : posts
+        'posts' : posts
     }
     return render(request, 'blog/blog-home.html', context)
 
