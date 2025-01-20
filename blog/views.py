fb02d063edb514dd11f654d401cb7f17249e0e61
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-20 20:46:10 +0330

    added pagination to single post page and proper post content up to this point to single post page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
@@ -21,5 +21,16 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single_view(request, pid):
-
-    return render(request, 'blog/single-post.html')
+    post = get_object_or_404(Post, id=pid, status=1,
+                             published_date__lte=timezone.now())
+    post.counted_views += 1
+    post.save()
+    other_posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).exclude(id=pid)
+    next_post = other_posts.filter(id__gt=pid).order_by('id').first()
+    previous_post = other_posts.filter(id__lt=pid).order_by('-id').first()
+    context = {
+        'post' : post,
+        'next' : next_post,
+        'previous' : previous_post
+    }
+    return render(request, 'blog/single-post.html', context)
