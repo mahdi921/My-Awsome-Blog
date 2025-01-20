052ef330821a15ccb5cff2c3d68e48e16fd956fa
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-20 20:59:12 +0330

    added image field to post table in database and migrated

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    #image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    image = models.ImageField(upload_to='blog/', default='blog/assets/img/blog/default.jpg')
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
