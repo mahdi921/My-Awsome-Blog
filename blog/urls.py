36d88e4490876c99dac08a9932f462fba35186d7
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-20 15:18:55 +0330

    moved website app urls to their right place

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path, re_path
-from blog.views import index_view, about_view, contact_view, blog_single_view, blog_view
+from django.urls import path
+from blog.views import blog_single_view, blog_view
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('about/', about_view, name='about'),
-    path('contact/', contact_view, name='contact'),
-    path('blog/', blog_view, name='blog'),
-    path('<int:pid>', blog_single_view, name='single'),
+    path('', blog_view, name='blog'),
+    path('<int:pid>/', blog_single_view, name='single'),
 ]
