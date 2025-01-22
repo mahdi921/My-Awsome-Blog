cb62d3b42ac8dfc3bd8c207609a76685a1332e2c
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-22 15:47:51 +0330

    added blog author custom inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,13 +1,16 @@
 from django import template
 from blog.models import Post
+from django.db.models import Count
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 register = template.Library()
-#
-# @register.inclusion_tag('blog/blog-author-widget.html')
-# def blog_author():
-#     author = User.objects.filter()
+
+@register.inclusion_tag('blog/blog-author-widget.html')
+def blog_author():
+    popular_author = Post.objects.filter(author__isnull=False).values('author').annotate(post_count=Count('id')).order_by('-post_count')[0]
+    author = User.objects.get(id=popular_author['author'])
+    return {'author': author}
 
 @register.inclusion_tag('blog/blog-popular-posts.html')
 def popular_widget(qty=5):
