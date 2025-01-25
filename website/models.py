1c726035c8efe0df550eac901c32b2eeb3b328a6
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-25 22:21:01 +0330

    added newsletter table to database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.email
