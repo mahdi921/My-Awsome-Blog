68bd24f303f69f4aad014dd13cd944b60cf4fa59
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-25 22:41:13 +0330

    added contact table to the database

--- website/models.py
+++ website/models.py
@@ -10,3 +10,17 @@ class Newsletter(models.Model):
 
     def __str__(self):
         return self.email
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(blank=False)
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    message = models.TextField(blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return f'{self.name} - {self.subject}'
