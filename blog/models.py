29954cb198f5db430b23672cd31051e1716770c7
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-25 16:05:46 +0330

    added comment table to database

--- blog/models.py
+++ blog/models.py
@@ -34,3 +34,19 @@ class Post(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse("blog:single", kwargs={"pid": self.id})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(blank=False)
+    subject = models.CharField(max_length=255, null=True, default='-empty-')
+    message = models.TextField(blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f'{self.name} - {self.post}'
