c836fe190acbc5186ddc173ad834737b3fb01fe1
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-26 20:13:32 +0330

    added index trending posts custom template tag

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -8,3 +8,8 @@ register = template.Library()
 def index_slider(arg=6):
     posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-created_date')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('website/index-trending-section.html')
+def index_trending(arg=5):
+    trending_posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-counted_views')[:arg]
+    return {'trending_posts':trending_posts}
