2a39ed7d402e240068c6b7fbfae6a653be60a44a
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-27 15:16:37 +0330

    added accounts url to projects urls file

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
