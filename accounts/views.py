06e6f59827a6f2bc1313869516207b36770b4b92
mahdi921 <mahdi.afshar921@gmail.com>
2025-01-27 18:13:57 +0330

    added logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +10,10 @@ def login_view(request):
 
 def register_view(request):
     return render(request, 'accounts/register.html')
+
+@login_required
+def logut_view(request):
+    logout(request)
+    messages.add_message(request, messages.SUCCESS,
+                         "You have been logged out, redirecting to home page...")
+    return redirect('/')
